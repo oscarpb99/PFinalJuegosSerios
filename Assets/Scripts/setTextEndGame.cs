b6861b9aa0dc2c8a3d792003509360c9b4bf0f9c
oscarpb99 <oscarped@ucm.es>
2024-11-27 20:13:51 +0100

    Cambio en el texto según la stat que llega a 0

--- Assets/Scripts/setTextEndGame.cs
+++ Assets/Scripts/setTextEndGame.cs
@@ -11,9 +11,14 @@ public class setTextEndGame : MonoBehaviour
     {
         if (GameManager.Instance.getWinCondition())
             GetComponent<TextMeshProUGUI>().text = "¡¡Has conseguido acabar la carrera!!";
-
-        else
-            GetComponent<TextMeshProUGUI>().text = "La carrera ha podido contigo";
+        else if(GameManager.Instance.getStat(0)<=0)
+            GetComponent<TextMeshProUGUI>().text = "Estas demasiado solo y te está afectando por lo que decides abandonar la carrera";
+        else if (GameManager.Instance.getStat(1) <= 0)
+            GetComponent<TextMeshProUGUI>().text = "El estrés y el cansancio generado por la carrera hacen que no puedas más y decides abandonarla";
+        else if (GameManager.Instance.getStat(2) <= 0)
+            GetComponent<TextMeshProUGUI>().text = "No tienes ganas de estudiar y decides abandonar la carrera";
+        else if (GameManager.Instance.getStat(3) <= 0)
+            GetComponent<TextMeshProUGUI>().text = "Te has quedado sin dinero y no puedes continuar la carrera";
 
 
 
